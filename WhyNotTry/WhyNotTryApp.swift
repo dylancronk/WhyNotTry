ce975d4839ab583c209d5add2a7662a2102e1049
Dylan Cronk <dylancronk.dev@gmail.com>
2024-05-31 07:46:34 -0400

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Dylan Cronk on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
